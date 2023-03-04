4549af809b8f64d38f186706a63a8985555d034d
admin-Komputer\admin <wagulec123@gmail.com>
2023-03-04 18:13:27 +0100

    initial commit

---
+++ lab1/zad1/library.h
@@ -0,0 +1,6 @@
+#ifndef ZAD1_LIBRARY_H
+#define ZAD1_LIBRARY_H
+
+void hello(void);
+
+#endif //ZAD1_LIBRARY_H
---
+++ lab1/zad1/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "library.c"
+int main(){
+    printf("hello world\n");
+    hello();
+    return 0;
+}
