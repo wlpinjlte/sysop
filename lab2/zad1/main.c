5c2aeec5bd82ef96e90a5b383abe5d618abae634
admin-Komputer\admin <wagulec123@gmail.com>
2023-03-13 23:38:48 +0100

    time added makeFile with test toDo

--- lab2/zad1/main.c
+++ lab2/zad1/main.c
@@ -1,8 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include<string.h>
+#include <string.h>
+#include <sys/times.h>
+double timeDifference(clock_t t1, clock_t t2){
+    return ((double)(t2 - t1) / sysconf(_SC_CLK_TCK));
+}
+
+void writeResult(clock_t start, clock_t end,struct tms t_start,struct tms t_end){
+    printf("\tREAL_TIME: %fl\n", timeDifference(start,end));
+    printf("\tUSER_TIME: %fl\n", timeDifference(t_start.tms_utime, t_end.tms_utime));
+    printf("\tSYSTEM_TIME: %fl\n", timeDifference(t_start.tms_stime, t_end.tms_stime));
+}
+
 void zad1a(char **argv){
     int we,wy;
     char c;
@@ -19,6 +30,7 @@ void zad1a(char **argv){
     close(we);
     close(wy);
 }
+
 void zad1b(char **argv){
     char c;
     FILE *we=fopen(argv[3],"r");
@@ -32,13 +44,29 @@ void zad1b(char **argv){
     fclose(we);
     fclose(wy);
 }
+
 int main(int argc,char **argv){
-//    printf("%d",argc);
+    clock_t timeStart,timeEnd;
+    static struct tms tmsStart,tmsEnd;
     if(argc!=5){
-        printf("zla ilosc argumentow");
+        printf("zla ilosc argumentow\n");
         return 1;
     }
-//    zad1a(argv);
+    printf("przyklad 1a\n");
+    timeStart=0;
+    timeEnd=0;
+    timeStart=times(&tmsStart);
+    zad1a(argv);
+    timeEnd=times(&tmsEnd);
+    writeResult(timeStart, timeEnd,
+                tmsStart, tmsEnd);
+    printf("przyklad 1b\n");
+    timeStart=0;
+    timeEnd=0;
+    timeStart=times(&tmsStart);
     zad1b(argv);
+    timeEnd=times(&tmsEnd);
+    writeResult(timeStart, timeEnd,
+                tmsStart, tmsEnd);
     return 0;
 }
--- lab2/zad2/main.c
+++ lab2/zad2/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 #include <sys/times.h>
-#define BLOCK_SIZE 1024
+#include <unistd.h>
+#define BLOCK_SIZE 1
 void reverse(char* buffer,int size_of_buffer){
     int i=0;
     char tmp;
@@ -13,11 +14,28 @@ void reverse(char* buffer,int size_of_buffer){
         buffer[size_of_buffer-1-i]=tmp;
     }
 }
+
+double timeDifference(clock_t t1, clock_t t2){
+    return ((double)(t2 - t1) / sysconf(_SC_CLK_TCK));
+}
+
+void writeResult(clock_t start, clock_t end,struct tms t_start,struct tms t_end){
+    printf("\tREAL_TIME: %fl\n", timeDifference(start,end));
+    printf("\tUSER_TIME: %fl\n", timeDifference(t_start.tms_utime, t_end.tms_utime));
+    printf("\tSYSTEM_TIME: %fl\n", timeDifference(t_start.tms_stime, t_end.tms_stime));
+}
+
 int main(int argc,char ** argv){
     if(argc!=3){
         printf("wrong number of arguments");
         return 1;
     }
+
+    clock_t timeStart,timeEnd;
+    static struct tms tmsStart,tmsEnd;
+    timeStart=0;
+    timeEnd=0;
+    timeStart=times(&tmsStart);
     FILE *read_file=fopen(argv[1],"r");
     FILE *write_file=fopen(argv[2],"w");
     int size_of_file=0;
@@ -34,5 +52,8 @@ int main(int argc,char ** argv){
     }
     reverse(buffer,strlen(buffer));
     fwrite(buffer,sizeof(char),size_of_file,write_file);
+    timeEnd=times(&tmsEnd);
+    writeResult(timeStart, timeEnd,
+                tmsStart, tmsEnd);
     return 0;
 }
