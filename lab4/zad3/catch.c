6e8a85e069fcf3094de59d17eca5d87088d00b10
Mateusz Waga-laptop <wplinjlte@gmail,com>
2023-03-29 17:08:40 +0200

    zad3 lab4-to check

--- lab4/zad3/catch.c
+++ lab4/zad3/catch.c
@@ -1,8 +1,103 @@
 #include<stdio.h>
+#include<signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+int parameter=0;
+int status=0;
+int counter=0;
 
+void handler(int signo,siginfo_t* siginfo,void * context){
+    printf("pid:%d\n",siginfo->si_pid);
+    status=1;
+    parameter=siginfo->si_status;
+    kill(siginfo->si_pid,SIGUSR1);
+    counter+=1;
+}
+
+void printCounter(){
+    fflush(stdout);
+    printf("counter:%d",counter);
+}
+
+void end(){
+    printf("end");
+    exit(0);
+}
+
+void for1to100(){
+    for(int i=1;i<=100;i++){
+        fflush(stdout);
+        printf("%d ",i);
+    }
+    printf("\n");
+    exit(0);
+}
+
+void printTime(){
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    fflush(stdout);
+    printf ( "Current local time and date: %s", asctime (timeinfo) );
+}
+
+void printEverySecond(){
+    while(parameter==4){
+        fflush(stdout);
+        printTime();
+        sleep(1);
+    }
+}
 
 int main(){
+    printf("pid: %d\n",getpid());
+    struct sigaction act;
+    sigemptyset(&act.sa_mask);
+    act.sa_sigaction=handler;
+    act.sa_flags=SA_SIGINFO;
+    sigaction(SIGUSR1,&act,NULL);
+    while(1){
+        if(status==0)
+            continue;
+        fflush(stdout);
+        status=0;
+        if(parameter==1){
+            printf("paramter1\n");
+            if(fork()==0){
+                signal(SIGUSR1,SIG_IGN);
+                for1to100();
+            }
+        }else if(parameter==2){
+            printf("parametr2\n");
+            if(fork()==0){
+                printTime();
+                exit(0);
+            }
 
+        }else if(parameter==3){
+            fflush(stdout);
+            printf("parametr3\n");
+            fflush(stdout);
+            if(fork()==0){
+                printCounter();
+                exit(0);
+            }
+
+        }else if(parameter==4){
+            printf("parametr4\n");
+            printEverySecond();
+        }else if(parameter==5){
+            printf("parametr5\n");
+            end();
+        }
+        fflush(stdout);
+
+    }
 
 
     return 0;
--- lab4/zad3/send.c
+++ lab4/zad3/send.c
@@ -3,32 +3,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <stdlib.h>
 int wyk=2;
 
 void licznik(int signo){
+    printf("sygnal przyszedl\n");
     wyk+=1;
 }
 
 int main(int argc,char **argv){
-    if(argc!=3){
-        printf("invalid number of arguments");
+    if(argc<3){
+        printf("invalid number of arguments\n");
         return 1;
     }
     int catch_pid=atoi(argv[1]);
     int function;
-    sigset_t set;
-    sigemptyset(&set);
-    sigaddset(&set,SIGUSR1);
+    signal(SIGUSR1,licznik);
+    printf("%d:\n",catch_pid);
+    fflush(stdout);
     for(int i=2;i<argc;i++){
         function=atoi(argv[i]);
-
-        signal(SIGUSR1,licznik);
-
         sigval_t val={function};
         sigqueue(catch_pid,SIGUSR1,val);
-
-        sigsuspend(set);
+        printf("signal send\n");
+        fflush(stdout);
+        pause();
+        fflush(stdout);
     }
+//    sleep(100);
     return 0;
 }
