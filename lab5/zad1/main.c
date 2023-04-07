f946d1cd93560de707a33e75c98a9e4c291f5e33
admin-Komputer\admin <wagulec123@gmail.com>
2023-04-08 00:05:17 +0200

    zad1 done

--- lab5/zad1/main.c
+++ lab5/zad1/main.c
@@ -5,16 +5,21 @@
 
 int main(int argc,char **argv){
     if(argc==2){
-        char c;
-        FILE* mail=popen("mail -p","r");
-        while((fread(&c,sizeof(char),1,mail))){
-            printf("%c",c);
+        FILE* mail;
+        if(strcmp(argv[1],"nadawca")==0){
+            mail=popen("mail | tail -n +2 | sort -k3d","w");
+        }else if(strcmp(argv[1],"data")==0){
+            mail=popen("mail | tail -n +2","w");
+        }else{
+            printf("invalid argument");
+            return 1;
         }
+        fwrite("exit",sizeof(char),5,mail);
         pclose(mail);
     }else if(argc==4){
         char command[1024];
         snprintf(command,1024,"mail -s \"%s\" %s",argv[2],argv[1]);
-        printf("%s\n",command);
+//        printf("%s\n",command);
         FILE* mail=popen(command,"w");
         fwrite(argv[3],sizeof(char),strlen(argv[3]),mail);
         pclose(mail);
