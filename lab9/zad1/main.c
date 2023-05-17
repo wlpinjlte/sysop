379edbd79a7862800807c43e1e16fd63a15eb394
Mateusz Waga-laptop <wplinjlte@gmail,com>
2023-05-17 23:04:13 +0200

    small changes

--- lab9/zad1/main.c
+++ lab9/zad1/main.c
@@ -7,17 +7,17 @@
 #define NUMBER_OF_ELVES 10
 #define NUMBER_OF_WAITING_ELVES 3
 
-int reindeers=0;
+int reindeer=0;
 int elves=0;
 long *waiting_elves_ids;
 int counter=0;
 
 pthread_mutex_t data_access_mutex=PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t waiting_reindeers=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t waiting_reindeer=PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t waiting_elves=PTHREAD_MUTEX_INITIALIZER;
 
 pthread_cond_t santa_condition=PTHREAD_COND_INITIALIZER;
-pthread_cond_t reindeers_condition=PTHREAD_COND_INITIALIZER;
+pthread_cond_t reindeer_condition=PTHREAD_COND_INITIALIZER;
 pthread_cond_t elves_condition=PTHREAD_COND_INITIALIZER;
 
 void *santa(void *arg){
@@ -25,19 +25,19 @@ void *santa(void *arg){
     while(counter<3){
         printf("Santa go to sleep\n");
         pthread_mutex_lock(&data_access_mutex);
-        while(reindeers<NUMBER_OF_REINDEER&&elves<NUMBER_OF_WAITING_ELVES){
+        while(reindeer<NUMBER_OF_REINDEER&&elves<NUMBER_OF_WAITING_ELVES){
             pthread_cond_wait(&santa_condition,&data_access_mutex);
         }
-        if(reindeers==NUMBER_OF_REINDEER){
+        if(reindeer==NUMBER_OF_REINDEER){
             printf("Santa wake up: delivering presents\n");
 //            pthread_mutex_unlock(&data_access_mutex);
             sleep(2 + rand() % 3);
 //            pthread_mutex_lock(&data_access_mutex);
             counter+=1;
-            reindeers=0;
-            pthread_mutex_lock(&waiting_reindeers);
-            pthread_cond_broadcast(&reindeers_condition);
-            pthread_mutex_unlock(&waiting_reindeers);
+            reindeer=0;
+            pthread_mutex_lock(&waiting_reindeer);
+            pthread_cond_broadcast(&reindeer_condition);
+            pthread_mutex_unlock(&waiting_reindeer);
         }else if(elves==NUMBER_OF_WAITING_ELVES){
 //            pthread_mutex_unlock(&data_access_mutex);
             sleep(1 + rand() % 2);
@@ -51,25 +51,26 @@ void *santa(void *arg){
         pthread_mutex_unlock(&data_access_mutex);
     }
     printf("santa closing\n");
+    return NULL;
 }
 
-void *reindeer(void *arg){
+void *reindeer_thread_function(void *arg){
     printf("Reindeer number %ld created\n",pthread_self());
     while(1){
         printf("Reindeer number %ld on vaction\n",pthread_self());
         sleep(5+(rand()%6));
         pthread_mutex_lock(&data_access_mutex);
-        reindeers+=1;
-        if(reindeers==NUMBER_OF_REINDEER){
+        reindeer+=1;
+        if(reindeer==NUMBER_OF_REINDEER){
             printf("Reindeer number %ld wake up santa\n",pthread_self());
         }else{
             printf("Reindeer number %ld waiting for santa\n",pthread_self());
         }
         pthread_cond_broadcast(&santa_condition);
         pthread_mutex_unlock(&data_access_mutex);
-        pthread_mutex_lock(&waiting_reindeers);
-        pthread_cond_wait(&reindeers_condition,&waiting_reindeers);
-        pthread_mutex_unlock(&waiting_reindeers);
+        pthread_mutex_lock(&waiting_reindeer);
+        pthread_cond_wait(&reindeer_condition,&waiting_reindeer);
+        pthread_mutex_unlock(&waiting_reindeer);
     }
 }
 
@@ -92,7 +93,7 @@ void *elf(void *arg){
             pthread_cond_wait(&elves_condition,&waiting_elves);
             pthread_mutex_unlock(&waiting_elves);
         }else{
-            printf("Elf number %ld handle thing yourself\n",pthread_self());
+            printf("Elf number %ld handle thing himself\n",pthread_self());
             pthread_mutex_unlock(&data_access_mutex);
         }
     }
@@ -105,7 +106,7 @@ int main(){
     pthread_t santa_thread;
     pthread_create(&santa_thread,NULL,santa,NULL);
     for(int i=0;i<NUMBER_OF_REINDEER;i++){
-        pthread_create(&reindeers_threads[i],NULL,reindeer,NULL);
+        pthread_create(&reindeers_threads[i],NULL,reindeer_thread_function,NULL);
     }
     for(int i=0;i<NUMBER_OF_ELVES;i++){
         pthread_create(&elves_threads[i],NULL,elf,NULL);
